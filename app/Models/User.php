a2c751e9daff0e37fc4afda7aa333a6ff5ad706d
chillag09733 <chillag09733@gmail.com>
2025-01-10 20:41:44 +0100

    login test

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,4 +35,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Order::class);
     }
+
+    public function isAdmin()  {
+        return $this->role === "A";
+    }
 }
--- database/factories/DictionaryFactory.php
+++ database/factories/DictionaryFactory.php
@@ -17,7 +17,7 @@ class DictionaryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+       //
         ];
     }
 }
