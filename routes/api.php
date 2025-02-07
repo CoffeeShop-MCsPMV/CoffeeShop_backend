e8c361baebd096920556d90b122f586cbc5e8747
chillag09733 <chillag09733@gmail.com>
2025-02-07 20:55:44 +0100

    összekapcsolás

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-Route::post('/register', [RegisteredUserController::class, 'store']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::post('/register', [RegisteredUserController::class, 'store']);
+// Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
 
 Route::middleware(['auth:sanctum'])
